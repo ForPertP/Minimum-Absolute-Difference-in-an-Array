a02d87e97daaa4e810d45a81b85ee0180c4093e8
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-07-15 07:43:20 +0900

    Update Minimum-Absolute-Difference-in-an-Array.cpp

--- Minimum-Absolute-Difference-in-an-Array.cpp
+++ Minimum-Absolute-Difference-in-an-Array.cpp
@@ -1,1 +1,17 @@
+int minimumAbsoluteDifference(vector<int> arr)
+{
+    std::sort(arr.begin(), arr.end());
+    int min = arr[1] - arr[0]; 
+
+    for (size_t i = 1; i < arr.size()-1; ++i)
+    { 
+        int differ = arr[i+1] - arr[i];
+        if (differ < min)
+        {
+            min = differ; 
+        }
+    }
+
+    return min;
+}
 
