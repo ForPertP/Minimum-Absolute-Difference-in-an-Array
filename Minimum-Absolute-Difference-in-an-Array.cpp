8dca395b4d14697216c352fa5e1330752bd42189
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-01-28 09:42:22 +0900

    Update Minimum-Absolute-Difference-in-an-Array.cpp

--- Minimum-Absolute-Difference-in-an-Array.cpp
+++ Minimum-Absolute-Difference-in-an-Array.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumAbsoluteDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
 int minimumAbsoluteDifference(vector<int> arr)
 {
     std::sort(arr.begin(), arr.end());
