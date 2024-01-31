fa166eea1cf3785dc76e7c74f138998c51c8882a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-02-01 08:29:10 +0900

    Update Minimum-Absolute-Difference-in-an-Array.java

--- Minimum-Absolute-Difference-in-an-Array.java
+++ Minimum-Absolute-Difference-in-an-Array.java
@@ -20,6 +20,19 @@ class Result {
      */
 
     public static int minimumAbsoluteDifference(List<Integer> arr) {
+        Collections.sort(arr); 
+        int min = arr.get(1) - arr.get(0); 
+
+        for (int i = 1; i < arr.size() - 1; ++i)
+        { 
+            int differ = arr.get(i+1) - arr.get(i);
+            if (differ < min)
+            {
+                min = differ; 
+            }
+        }
+
+        return min;
     }
 
 }
